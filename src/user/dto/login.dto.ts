a8824fb361a62b9f21df7737c8e8b5e7ee3ba128
komiharuu <biomilky3@gmail.com>
2024-07-08 12:10:51 +0900

    파이프 일부 수정

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 import { Role } from '../types/userRole.type';
 
 export class LoginDto {
@@ -8,11 +14,10 @@ export class LoginDto {
 
   @IsString()
   @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
-  // @Min(5, { message: '비밀번호는 5자 이상으로 입력해주세요' })
+  @MinLength(5, { message: '비밀번호는 5자 이상으로 입력해주세요' })
   password: string;
 
-  // @IsString()
-  // @IsNotEmpty({ message: '비밀번호를 다시 확인해주세요' })
+  @IsOptional({ message: '비밀번호를 다시 확인해주세요' })
   passwordConfirm: string;
 
   // @IsString()
